18eae437a994881082b671e60f76d5a7bb077edc
Alan Vinicius Secundo <alan_secundo@hotmail.com>
2020-11-19 22:29:56 -0300

    First Commit with Starter Header and styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+  ],
 }
