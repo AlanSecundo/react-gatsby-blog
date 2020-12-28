c2f21590d9c7a0784d0d1fc870e51fd72badc87d
Alan Secundo <alan_secundo@hotmail.com>
2020-12-28 16:39:17 -0300

    Generate sitemap.xml and robot.txt files

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,45 @@ module.exports = {
     `gatsby-plugin-dark-mode`,
     `gatsby-plugin-modal-routing`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+        }`,
+        resolveSiteUrl: ({site}) => {
+          return site.siteMetadata.siteUrl
+        },
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.nodes.map(node => {
+            return {
+              url: `${site.siteMetadata.siteUrl}${node.path}`,
+              changefreq: `weekly`,
+              priority: 0.5,
+            }
+          })
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://alansecundosite.gtsb.io/',
+        sitemap: 'https://alansecundosite.gtsb.io/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
